d41b844f9d291e8bb941da2db717eecc7b3d5d40
sramos <sramos@student.codam.nl>
2024-11-10 16:17:40 +0100

    Start expansion

--- src/parsing/parsing.c
+++ src/parsing/parsing.c
@@ -6,7 +6,7 @@
 /*   By: sramos <sramos@student.42.fr>                +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/09/23 11:59:18 by sramos        #+#    #+#                 */
-/*   Updated: 2024/11/08 16:19:07 by sramos        ########   odam.nl         */
+/*   Updated: 2024/11/10 13:50:52 by sramos        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,67 +48,73 @@ void	parsing(t_data *data, char **envp)
 			ft_free_string(data->line);
 		data->line = readline("minishell:~$ ");
 		// printf("data->line: %s\n", data->line);
+		if (!data->line[0])
+			continue;
 		if (!data->line)
 			error_exit(data, NULL, "exit\n", 0);
-		if (data->line[0])
-			add_history(data->line);
-		// parse_envp(data, envp); //This is resulting in segfault.
-		if (input_checker(data) == 0)
+		else
 		{
-			expansion(data);
-			token_list = tokenization(data, token_list);
-			
-			/*----------------------------------TEMP----------------------------------------------*/
-			// t_token *current = token_list;
-			// while (current)
-			// {
-			// 	if (current->type == T_PIPE)
-			// 		printf("\n");
-			// 	printf("LEN: %i - TYPE: %i - STR: %s\n", current->lenght, current->type, current->str);
-			// 	current = current->next;
-			// }
-			/*----------------------------------TEMP----------------------------------------------*/
-			parse_input(data, token_list);
-			data->cmd_current = data->cmd_head;
-			/*----------------------------------TEMP----------------------------------------------*/
-			// t_cmd *currentll = data->cmd_head;
-			// while (currentll != NULL)
-			// {
-			// 	int i = 0;
-			// 	if (currentll->cmd)
-			// 	{
-			// 		while(currentll->cmd[i])
-			// 		{
-			// 			printf("This is cmd[%i]: %s - %p\n", i, currentll->cmd[i], currentll->cmd[i]);
-			// 			i++;
-			// 		}
-			// 	}
-			// 	printf("This is fd_in: %i\n", currentll->fd_in);
-			// 	printf("This is fd_out: %i\n", currentll->fd_out);
-			// 	printf("This is infile: %s\n", currentll->infile);
-			// 	printf("This is outfile: %s\n\n\n", currentll->outfile);
-			// 	printf("Nbr pipes: %i\n", data->nbr_pipes);
-			// 	// currentll = currentll->pipe;
-			// }
-			/*----------------------------------TEMP----------------------------------------------*/
-			// close (data->cmd_head->fd_in);
-			if (token_list)
+			if (data->line[0])
+				add_history(data->line);
+			// parse_envp(data, envp); //This is resulting in segfault.
+			if (input_checker(data) == 0)
 			{
-				free_token_list(token_list);
-				token_list = NULL;
-			}
-			// if (exec(data) == 9)
-			// 	return ;
-			if (data->cmd_head)
-			{
-				free_cmd_list(data->cmd_head);
-				data->cmd_head = NULL;
-			}
-			data->nbr_pipes = 0;
-			data->std[IN] = STDIN_FILENO;
-			data->std[OUT] = STDOUT_FILENO;
-			// should stay (for the moment commented out - infinite loop)
-			wait(NULL);
+				expansion(data);
+				token_list = tokenization(data, token_list);
+				
+				/*----------------------------------TEMP----------------------------------------------*/
+				// t_token *current = token_list;
+				// while (current)
+				// {
+				// 	if (current->type == T_PIPE)
+				// 		printf("\n");
+				// 	printf("LEN: %i - TYPE: %i - STR: %s\n", current->lenght, current->type, current->str);
+				// 	current = current->next;
+				// }
+				/*----------------------------------TEMP----------------------------------------------*/
+				parse_input(data, token_list);
+				data->cmd_current = data->cmd_head;
+				printf("DATA->LINE: %s, %p\n", data->line, data->line);
+				/*----------------------------------TEMP----------------------------------------------*/
+				t_cmd *currentll = data->cmd_head;
+				while (currentll != NULL)
+				{
+					int i = 0;
+					if (currentll->cmd)
+					{
+						while(currentll->cmd[i])
+						{
+							printf("This is cmd[%i]: %s - %p\n", i, currentll->cmd[i], currentll->cmd[i]);
+							i++;
+						}
+					}
+					printf("This is fd_in: %i\n", currentll->fd_in);
+					printf("This is fd_out: %i\n", currentll->fd_out);
+					printf("This is infile: %s\n", currentll->infile);
+					printf("This is outfile: %s\n\n\n", currentll->outfile);
+					printf("Nbr pipes: %i\n", data->nbr_pipes);
+					currentll = currentll->pipe;
+				}
+				/*----------------------------------TEMP----------------------------------------------*/
+				// close (data->cmd_head->fd_in);
+				if (token_list)
+				{
+					free_token_list(token_list);
+					token_list = NULL;
+				}
+				if (exec(data) == 9)
+					return ;
+				if (data->cmd_head)
+				{
+					free_cmd_list(data->cmd_head);
+					data->cmd_head = NULL;
+				}
+				data->nbr_pipes = 0;
+				data->std[IN] = STDIN_FILENO;
+				data->std[OUT] = STDOUT_FILENO;
+				// should stay (for the moment commented out - infinite loop)
+				wait(NULL);
+		}
 		}
 	}
 }
