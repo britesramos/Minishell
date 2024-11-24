a0703bc03a13f16580e26233f32154012265551b
Malgorzata Stencel <mstencel@f0r3s22.codam.nl>
2024-11-24 13:39:52 +0100

    fds in single cmd

--- src/execution/pipes/single_cmd.c
+++ src/execution/pipes/single_cmd.c
@@ -6,7 +6,7 @@
 /*   By: mstencel <mstencel@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/10/22 07:41:56 by mstencel      #+#    #+#                 */
-/*   Updated: 2024/11/24 11:06:51 by mstencel      ########   odam.nl         */
+/*   Updated: 2024/11/24 13:38:56 by mstencel      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,16 @@
 /// @brief dup2() for redirection, does nothing if in/out are STD IN/OUT.
 /// From man dup2: If oldfd is a valid file descriptor, and newfd has the same
 /// value as oldfd, then dup2() does nothing, and returns newfd.
-static void	ft_dup_all(t_cmd *current, t_data *data)
+static int	ft_dup_all(t_cmd *current, t_data *data)
 {
+	if (current->fd_in == -1 || current->fd_out == -1)
+		return (EXIT_FAILURE);
 	if (current->fd_in != data->std[IN])
 	{
 		if (dup2(current->fd_in, data->std[IN]) == -1)
 		{
 			perror("dup2 fd_in in single cmd");
-			return ;
+			return (EXIT_FAILURE);
 		}
 		close_fd(&current->fd_in);
 	}
@@ -31,10 +33,11 @@ static void	ft_dup_all(t_cmd *current, t_data *data)
 		if (dup2(current->fd_out, data->std[OUT]) == -1)
 		{
 			perror("dup2 fd_out in single cmd");
-			return ;
+			return (EXIT_FAILURE);
 		}
 		close_fd(&current->fd_out);
 	}
+	return (EXIT_SUCCESS);
 }
 
 static void	ft_single_child(t_data *data)
@@ -69,7 +72,11 @@ void	single_cmd(t_data *data)
 	}
 	if (pid == 0)
 	{
-		ft_dup_all(data->cmd_current, data);
+		if (ft_dup_all(data->cmd_current, data) == EXIT_FAILURE)
+		{
+			clean_up(data);
+			exit (EXIT_FAILURE);
+		}
 		ft_single_child(data);
 	}
 	ms_signals(NONINTERACTIVE);
